f770f8ce75a1ed4047227e612583decd42cd5999
Prince Nmezi <stephenprince427@gmail.com>
2023-07-07 23:17:49 +0100

    home style fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/images/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container }>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Enhance the design of your digital product </h1>
+        <p className={styles.description}>Turning your idea into Reality. We bring together the teams from the global tech industry</p>
+        <button className={styles.btn}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+      <Image src={Hero} alt='hero' className={styles.img } />
+      </div>
+    
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Link from 'next/link'
+import {BsFacebook, BsTwitter, BsLinkedin,BsInstagram} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-      <div>
+      <div className={styles.container}>
           <div>@ 2023 Isomia. All rights reserved</div>
+          <div className={styles.social}>
+              <Link href="https://facebook.com"><BsFacebook size={22} /></Link>
+              <Link href="https://twitter.com"><BsTwitter size={22}/></Link>
+              <Link href="https://linkedln.com"><BsLinkedin size={22}/></Link>
+              <Link href="https://instagram.com"><BsInstagram size={22}/></Link>
+          </div>
     </div>
   )
 }
