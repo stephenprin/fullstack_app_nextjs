cb445facac309dfdecd365b4b2fb8414b3c7f10b
Prince Nmezi <stephenprince427@gmail.com>
2023-07-08 00:50:44 +0100

    About UI

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import styles from './page.module.css'
+import People from 'public/images/people.jpg'
+import Image from 'next/image'
+import Button from '@/components/button/Button'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.container}>
+          <div className={styles.imgContainer}>
+              <Image src={People} fill={true} alt="people" className={styles.img} />
+              <div className={styles.imgText}>
+                  <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+                  <p className={styles.imgDes}>Handcrafting award winning digital experience</p>
+              </div>
+          </div>
+          <div className={styles.textContainer}>
+            <div className={styles.item}>
+                    <h1 className={styles.title}>Who Are We?</h1>
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quia, excepturi. Sequi accusantium itaque error beatae excepturi autem?
+                        Soluta dolorem et oluta dolorem et
+                        reprehenderit, ea distinctio quis?
+                        Atque laboriosam adipisci optio nisi magni?
+                        reprehenderit, ea distinctio quis?
+                      Atque laboriosam adipisci optio nisi magni?
+                      <br/>
+                      <br/>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quia, excepturi. Sequi accusantium itaque error beatae excepturi autem?
+                        Soluta dolorem et oluta dolorem et
+                        reprehenderit, ea distinctio quis?
+                        Atque laboriosam adipisci optio nisi magni?
+                        reprehenderit, ea distinctio quis?
+                        Atque laboriosam adipisci optio nisi magni?
+                  </p>
+              </div>
+              <br /> 
+              <br />
+              <div className={styles.item}>
+                    <h1 className={styles.title}>What We Do?</h1>
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quia, excepturi. Sequi accusantium itaque error beatae excepturi autem?
+                      Soluta dolorem et <br /> <br /> - Dynamic web application -Fast and reliable
+                      <br /> - Universal - All round the globe
+                      <br /> - Fast and Handy -Fast and reliable
+                      <br /> - Mobile Apps and Web3
+                  </p>
+                  <Button url="contact" text="Contact"/>
+              </div>
+              
+          </div>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from 'public/images/hero.png'
+import Button from '@/components/button/Button'
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <div className={styles.item}>
         <h1 className={styles.title}>Enhance the design of your digital product </h1>
         <p className={styles.description}>Turning your idea into Reality. We bring together the teams from the global tech industry</p>
-        <button className={styles.btn}>See Our Works</button>
+        <Button url="portfolio" text="See Our Works" />
       </div>
       <div className={styles.item}>
       <Image src={Hero} alt='hero' className={styles.img } />
