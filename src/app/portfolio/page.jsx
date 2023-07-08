aa2dfac83554bdf1f2ac04950f2ed2407294760f
Prince Nmezi <stephenprince427@gmail.com>
2023-07-09 00:42:23 +0100

    portfolio layout

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const Portfolio = () => {
   return (
-    <div>
-      Portfolio
+    <div className={styles.container}>
+      <div className={styles.selectTitle}>Choose a Project</div>
+      
+      <div className={styles.items}>
+        <Link href='/portfolio/websites' className={ styles.item}>
+              <span className={ styles.title}>Websites</span>
+        </Link>
+        <Link href='/portfolio/illustration' className={ styles.item}>
+          <span className={ styles.title}> Illustration</span>
+        </Link>
+        <Link href='/portfolio/application' className={ styles.item}>
+              <span className={ styles.title}>Applications</span>
+          </Link>
+      </div>
     </div>
   )
 }
