d1ff1294aff6c6ffc1d20e41119dbfeb9c692d09
Prince Nmezi <stephenprince427@gmail.com>
2023-07-09 00:16:38 +0100

    Contact UI and animation

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import styles from './page.module.css'
+import ContactForm from 'public/images/contact.png' 
+import Button from '@/components/button/Button'
+import Image from 'next/image'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+      <div className={styles.title}>Let's Keep iin Touch</div>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+
+          <Image src={ ContactForm} alt="contact" fill={true} className={styles.image } />
+
+        </div>
+        <form className={styles.form}>
+          <input type="text" className={styles.input} placeholder="Name" />
+          <input type="text" className={styles.input} placeholder="Email" />
+          <textarea className={styles.textarea} placeholder="Message" cols='30' rows='10' />
+          <Button url="#" text='Send' />
+        </form>
+       </div>
+
+    </div>
   )
 }
 
