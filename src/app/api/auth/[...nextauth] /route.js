0ede22a0db786dfc00c1b17ff59873d050bf8677
Prince Nmezi <stephenprince427@gmail.com>
2023-07-19 17:00:20 +0100

    user authentication and ptotected route

--- src/app/api/auth/[...nextauth] /route.js
+++ src/app/api/auth/[...nextauth] /route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-
+import CredentialsProvider from "next-auth/providers/credentials"
+import connect from "@/utils/db";
+import User from "@/models/User";
+import bcrypt  from "bcryptjs"
 
 const handler = NextAuth({
     // Configure one or more authentication providers
@@ -9,8 +12,38 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
+        CredentialsProvider({
+            id: "credentials",
+            name: "credentials",
+            async authorize(credentials) { 
+                await connect()
+
+                try {
+                    const user = await User.findOne({ email: credentials.email })
+                    if (user) {
+                        //check password
+                        const isPasswordCorrect = await bcrypt.compare(credentials.password, (await user).password)
+                        if (isPasswordCorrect) {
+                            return user
+                        } else {
+                            throw new Error("Wrong credentials")
+                        }
+
+                    } else {
+                        throw new Error("User not found")
+                    }
+                    
+                } catch (error) {
+                    throw new Error(error)
+                }
+
+            }
+            
+        })
         // ...add more providers here
     ],
-    logging: true,
+    pages: {
+        error: "/dashboard/login"
+    }
 });
 export { handler as GET, handler as POST }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,19 +3,35 @@ import useSWR from 'swr'
 import React from 'react'
 import styles from './page.module.css'
 import { useSession} from "next-auth/react"
+import { useRouter } from 'next/navigation'
+
 
 
 const Dashboard = () => {
   const session = useSession()
-  console.log(session)
+  const router = useRouter()
+
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher); 
   
-  return (
-    <div className={styles.container}>
+  if (session.status === 'loading') {
+    <p>Loading....</p>
+    
+  }
+  if (session.status === 'unauthenticated') { 
+    router?.push("/dashboard/login")
+  }
+  
+  
+
+    if (session.status === 'authenticated') { 
+      return (
+        <div className={styles.container}>
       Dashboard
    </div>
- )
+     )
+    }
+ 
   
 }
 
